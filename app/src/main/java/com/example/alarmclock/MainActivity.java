74635bd5725e2452c370b27e2c130366079b66e2
hidjlove1 <61003736+hidjlove1@users.noreply.github.com>
2020-05-21 15:16:46 +0900

    22DIA

--- app/src/main/java/com/example/alarmclock/MainActivity.java
+++ app/src/main/java/com/example/alarmclock/MainActivity.java
@@ -14,7 +14,8 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends Activity implements OnClickListener {
-    Button btnSetAlarm;
+    Button btnSetAlarm21;
+    Button btnSetAlarm22;
  //   EditText etHour, etMinute;
     int minute, hour;
     Calendar cal;
@@ -23,10 +24,12 @@ public class MainActivity extends Activity implements OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnSetAlarm = (Button) findViewById(R.id.Button_21);
+        btnSetAlarm21 = (Button) findViewById(R.id.Button_21);
+        btnSetAlarm22 = (Button) findViewById(R.id.Button_22);
       //  etHour = (EditText) findViewById(R.id.etHour);
        // etMinute = (EditText) findViewById(R.id.etMinute);
-        btnSetAlarm.setOnClickListener(this);
+        btnSetAlarm21.setOnClickListener(this);
+        btnSetAlarm22.setOnClickListener(this);
 
     }
 
@@ -41,6 +44,10 @@ public class MainActivity extends Activity implements OnClickListener {
         switch (v.getId()) {
             case R.id.Button_21:
                 set21dia();
+                break;
+
+            case R.id.Button_22:
+                set22dia();
                 break;
         }
     }
@@ -69,6 +76,44 @@ public class MainActivity extends Activity implements OnClickListener {
         k.putExtra(AlarmClock.EXTRA_MINUTES, 9);
         k.putExtra(AlarmClock.EXTRA_SKIP_UI, true);
         startActivity(k);
+    }
+
+    private void set22dia() {
+//        cal = new GregorianCalendar();
+//        cal.setTimeInMillis(System.currentTimeMillis());
+//        day = cal.get(Calendar.DAY_OF_WEEK);
+//        hour = cal.get(Calendar.HOUR_OF_DAY);
+//        minute = cal.get(Calendar.MINUTE);
+
+        Intent i = new Intent(AlarmClock.ACTION_SET_ALARM);
+        i.putExtra(AlarmClock.EXTRA_HOUR, 6);
+        i.putExtra(AlarmClock.EXTRA_MINUTES, 58);
+        i.putExtra(AlarmClock.EXTRA_SKIP_UI, true);
+        startActivity(i);
+
+        Intent j = new Intent(AlarmClock.ACTION_SET_ALARM);
+        j.putExtra(AlarmClock.EXTRA_HOUR, 9);
+        j.putExtra(AlarmClock.EXTRA_MINUTES, 15);
+        j.putExtra(AlarmClock.EXTRA_SKIP_UI, true);
+        startActivity(j);
+
+        Intent k = new Intent(AlarmClock.ACTION_SET_ALARM);
+        k.putExtra(AlarmClock.EXTRA_HOUR, 12);
+        k.putExtra(AlarmClock.EXTRA_MINUTES, 57);
+        k.putExtra(AlarmClock.EXTRA_SKIP_UI, true);
+        startActivity(k);
+
+        Intent l = new Intent(AlarmClock.ACTION_SET_ALARM);
+        l.putExtra(AlarmClock.EXTRA_HOUR, 13);
+        l.putExtra(AlarmClock.EXTRA_MINUTES, 39);
+        l.putExtra(AlarmClock.EXTRA_SKIP_UI, true);
+        startActivity(l);
+
+        Intent m = new Intent(AlarmClock.ACTION_SET_ALARM);
+        m.putExtra(AlarmClock.EXTRA_HOUR, 14);
+        m.putExtra(AlarmClock.EXTRA_MINUTES, 21);
+        m.putExtra(AlarmClock.EXTRA_SKIP_UI, true);
+        startActivity(m);
     }
 
 
